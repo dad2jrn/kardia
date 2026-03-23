5175ec080b44064d49fe478f3e0cf3064c6e6160
dad2jrn <dad2jrn@gmail.com>
2026-03-23 14:49:04 -0400

    configure HashRouter with crucifixion-date route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import CrucifixionDate from './pages/crucifixion-date'
 
 export default function App() {
-  return <Home />
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/crucifixion-date" element={<CrucifixionDate />} />
+      </Routes>
+    </HashRouter>
+  )
 }
