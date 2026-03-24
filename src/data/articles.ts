78262f6dedaf4ec6848030dda1ccaaf9a3594a85
dad2jrn <dad2jrn@gmail.com>
2026-03-23 20:09:56 -0400

    refactor: comment out unused test article in articles data

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -32,17 +32,17 @@ const articles: Article[] = [
     description:
       'A careful examination of the astronomical, calendrical, and historical evidence for the date of the crucifixion.',
   },
-  {
-    id: 'test',
-    order: 3,
-    href: '/#',
-    isRoute: true,
-    navLabel: 'Some cool article title',
-    label: 'Nifty Label',
-    title: 'I pitty the fool who does not click this link',
-    description:
-      'some description that is really long and really good and really interesting and really makes you want to click the link and read the article because it is so good and so interesting and so long that you just have to read it',
-  },
+  // {
+  //   id: 'test',
+  //   order: 3,
+  //   href: '/#',
+  //   isRoute: true,
+  //   navLabel: 'Some cool article title',
+  //   label: 'Nifty Label',
+  //   title: 'I pitty the fool who does not click this link',
+  //   description:
+  //     'some description that is really long and really good and really interesting and really makes you want to click the link and read the article because it is so good and so interesting and so long that you just have to read it',
+  // },
 ]
 
 export default [...articles].sort((a, b) => a.order - b.order)
