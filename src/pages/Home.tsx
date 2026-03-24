ebb91782c80f6683e04345ef3f1d09ce22a2a014
dad2jrn <dad2jrn@gmail.com>
2026-03-23 21:10:17 -0400

    use start here image from the interwebs instead

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,33 +13,7 @@ export default function Home() {
       <div className="articles">
         <div className="article-first-wrap">
           <div className="start-here-annotation">
-            <svg width="182" height="84" viewBox="0 0 182 84" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <defs>
-                <marker id="start-arrow-head" markerWidth="9" markerHeight="7" refX="9" refY="3.5" orient="auto">
-                  <polygon points="0 0, 9 3.5, 0 7" fill="#d4a017" fillOpacity="0.85" />
-                </marker>
-              </defs>
-              <path
-                d="M 140,26 C 125,8 98,52 104,78"
-                stroke="#d4a017"
-                strokeWidth="1.5"
-                strokeOpacity="0.85"
-                fill="none"
-                markerEnd="url(#start-arrow-head)"
-              />
-              <text
-                x="176"
-                y="20"
-                textAnchor="end"
-                fill="#d4a017"
-                fillOpacity="0.85"
-                fontFamily="Georgia, serif"
-                fontSize="14"
-                fontStyle="italic"
-              >
-                Start here!
-              </text>
-            </svg>
+            <img src="/start-here.png" alt="Start here!" className="start-here-img" />
           </div>
           <ArticleCard key={first.id} {...first} />
         </div>
