fafa4c844c124b862f29cbe551309fd36ad2b1ce
dad2jrn <dad2jrn@gmail.com>
2026-03-23 21:18:32 -0400

    fix: refactor image source as imported module to let vite handle the path.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import SiteNav from '../components/SiteNav'
 import Hero from '../components/Hero'
 import ArticleCard from '../components/ArticleCard'
 import articles from '../data/articles'
+import startHereImg from '/start-here.png'
 
 const [first, ...rest] = articles
 
@@ -13,7 +14,7 @@ export default function Home() {
       <div className="articles">
         <div className="article-first-wrap">
           <div className="start-here-annotation">
-            <img src="/start-here.png" alt="Start here!" className="start-here-img" />
+            <img src={startHereImg} alt="Start here!" className="start-here-img" />
           </div>
           <ArticleCard key={first.id} {...first} />
         </div>
